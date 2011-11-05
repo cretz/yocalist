c8c6c82321d57e23e504406be71deb9ac9840eef
Chad Retz <chad.retz@gmail.com>
2011-11-05 00:50:16 -0500

    Initial commit

---
+++ src/com/yocalist/client/Yocalist.java
@@ -0,0 +1,12 @@
+package com.yocalist.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class Yocalist implements EntryPoint {
+
+    @Override
+    public void onModuleLoad() {
+        
+    }
+    
+}
